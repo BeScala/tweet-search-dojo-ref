76ea4006b1482a6a4b3d88038b61c6ac4fcef443
Renato Cavalcanti <renato@xavena.eu>
2012-05-18 15:28:01 +0200

    implement new/update form

--- app/controllers/TweetController.scala
+++ app/controllers/TweetController.scala
@@ -1,10 +1,13 @@
 package controllers
 
 import play.api.mvc._
-import collection.mutable.ListBuffer
+
 import service.TweetProvider
 import play.api.libs.concurrent.Promise
 import model.{Tweet, SearchTerm}
+import play.api.data.Form
+import play.api.data.Forms._
+
 
 object TweetController extends Controller {
 
@@ -18,28 +21,45 @@ object TweetController extends Controller {
   /**
    * Saves a new SearchTerm. Handles form submission.
    */
-  def save = TODO
+  def save = Action { implicit request =>
+    searchTermForm.bindFromRequest.fold(
+      errors => { BadRequest(views.html.edit(errors)) },
+      term => {
+        term.save()
+        Redirect(routes.TweetController.list())
+      }
+    )
+  }
 
   /**
    * Displays search results for passed SearchTerm
    */
   def view(id: Long) = TODO
 
+  private val searchTermForm = Form {
+    mapping(
+      "id" -> longNumber,
+      "text" -> nonEmptyText)(SearchTerm.apply)(SearchTerm.unapply)
+  }
+
   /**
    * Prepares an empty form for new SearchTerm
    */
-  def newForm = TODO
+  def newForm = toFormPage(searchTermForm.fill(SearchTerm("")))
 
   /**
    * Fill form with existing SearchTerm
    */
-  def editForm(id: Long) = TODO
+  def editForm(id: Long) = toFormPage(searchTermForm.fill(SearchTerm.findById(id)))
 
+  private def toFormPage(form: Form[SearchTerm]) = Action(Ok(views.html.edit(form)))
 
   def tweets(searchTerm: String, max: Int) = Action {
     Async {
       val promiseOfTweets: Promise[List[Tweet]] = TweetProvider.fetchTweets(searchTerm, max)
-      promiseOfTweets.map { tweets => Ok(views.html.tweets(tweets))}
+      promiseOfTweets.map {
+        tweets => Ok(views.html.tweets(tweets))
+      }
     }
   }
 
--- app/model/SearchTerm.scala
+++ app/model/SearchTerm.scala
@@ -1,10 +1,31 @@
 package model
 
 import collection.mutable.ListBuffer
+import java.util.UUID
+import java.util.concurrent.atomic.AtomicLong
 
-case class SearchTerm(id:Long, text:String)
+case class SearchTerm(id: Long, text: String)
 
 object SearchTerm {
-  val terms  = new ListBuffer[SearchTerm]
-  def all : Seq[SearchTerm] = List(terms:_*)
+  val atomicInteger = new AtomicLong(1)
+
+  val terms = new ListBuffer[SearchTerm]
+
+  def save(searchTerm: SearchTerm): SearchTerm = {
+    findByIdOpt(searchTerm.id).foreach(s => terms -= s)
+    terms += searchTerm
+    searchTerm
+  }
+
+  def apply(term: String): SearchTerm = new SearchTerm(atomicInteger.getAndIncrement, term)
+
+  def all: Seq[SearchTerm] = List(terms: _*)
+
+  def findByIdOpt(id: Long): Option[SearchTerm] = terms.find(e => e.id == id)
+
+  def findById(id: Long): SearchTerm = terms.find(e => e.id == id).get
+
+  implicit def toActiveRecord(searchTerm: SearchTerm) = new {
+    def save() = SearchTerm.save(searchTerm)
+  }
 }
