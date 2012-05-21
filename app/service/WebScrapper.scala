6e690d69eb425fab3b7f28f667ab22c3129b6348
Renato Cavalcanti <renato@xavena.eu>
2012-05-21 09:34:08 +0200

    enabled WebScrapper

--- app/service/WebScrapper.scala
+++ app/service/WebScrapper.scala
@@ -2,38 +2,37 @@ package service
 
 import model.{Tweet, TweetNews}
 import org.jsoup.Jsoup
+import play.api.Logger
 
 
 object WebScrapper {
 
   def scrapp(tweet: Tweet): List[TweetNews] = {
-    for { url <- tweet.urls }
-      yield  {
-      // we just need a dumb unique id
-      val id = url.hashCode + "-" + tweet.id
-    TweetNews(id,
-              "fake title",
-              tweet.content,
-              tweet.user, tweet.imgUrl, tweet.date, url, tweet.tags)
-    }
+    val list =
+      for { url <- tweet.urls }
+      yield {
+        try {
+          val doc = Jsoup.connect(url).get
+
+          val content =
+            if (doc.getElementById("content") == null)
+              doc.getElementById("content").text
+            else
+              doc.body.text
+
+          // we just need a dumb unique id
+          val id = url.hashCode + "-" + tweet.id
+
+          TweetNews(id,
+                    doc.title, content,
+                    tweet.user, tweet.imgUrl,
+                    tweet.date,
+                    url, tweet.tags)
+        } catch {
+          case _ => Logger.warn("Couldn't scrap url " + url)
+        }
+      }
+
+      list.collect { case t: TweetNews => t }
   }
-//    for { url <- tweet.urls }
-//    yield {
-//      val doc = Jsoup.connect(url).get
-//      val content =
-//        if (doc.getElementById("content") == null)
-//          doc.getElementById("content").text
-//        else
-//          doc.body.text
-//
-//      // we just need a dumb unique id
-//      val id = url.hashCode + "-" + tweet.id
-//
-//      TweetNews(id,
-//                doc.title, content,
-//                tweet.user, tweet.imgUrl,
-//                tweet.date,
-//                url, tweet.tags)
-//    }
-//  }
 }
--- app/service/akka/WebScrapperWorker.scala
+++ app/service/akka/WebScrapperWorker.scala
@@ -3,15 +3,16 @@ package service.akka
 import akka.actor.Actor
 import service.WebScrapper
 import play.api.Logger
+import model.TweetNews
 
 
 class WebScrapperWorker extends Actor {
 
   protected def receive: Receive = {
-    case msg : WebScrap => {
+    case msg: WebScrap => {
       Logger.info("[" + this + "] - received assignement for tweet id:" + msg.tweet.id)
       val tweetNews = WebScrapper.scrapp(msg.tweet)
-      tweetNews.foreach { _.save }
+      tweetNews.foreach { _.save  }
       sender ! FinishedWebScrapping(tweetNews)
     }
   }
